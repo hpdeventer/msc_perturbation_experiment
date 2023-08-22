0562530b0afa45cc615459d4b7ce00d2b0256a0e
Heinrich van Deventer <51166531+hpdeventer@users.noreply.github.com>
2023-08-22 21:59:43 +0200

    fully parallel

--- perturb_experiment_main.py
+++ perturb_experiment_main.py
@@ -31,7 +31,7 @@ warnings.filterwarnings('ignore')
 
 # Execute the experiment
 #basic_perturbation_experiment(**experiment_arguments)
-
+'''
 import time
 from multiprocessing import Pool
 from datetime import datetime
@@ -97,3 +97,88 @@ if __name__ == "__main__":
     parallel_execute()
 
     print(f"Total Execution Time: {time.time() - overall_start_time} seconds")
+'''
+
+import time
+import numpy as np
+import os
+from multiprocessing import Pool
+import gc
+
+def wrapper(args):
+    basic_perturbation_experiment(**args)
+
+def aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial_numbers):
+    aggregated_results = {}
+
+    for trial in trial_numbers:
+        for optimizer in optimizers:
+            for use_pseudorehearsal in pseudorehearsals:
+                base_folder = f"results/input_dim_{input_dim}_partition_num_{partition}_{use_pseudorehearsal}_{optimizer}/trial_{trial}"
+
+                key = f"{input_dim}_{partition}_{use_pseudorehearsal}_{optimizer}"
+                if key not in aggregated_results:
+                    aggregated_results[key] = {}
+                aggregated_results[key][trial] = {}
+                
+                for i in range(partition):
+                    for data_name, file_name in [('model_output_prior_training', 'output_prior_training.npy'),
+                                                 ('model_output_after_training', 'output_after_training.npy'),
+                                                 ('absolute_perturbation', 'absolute_perturbation.npy')]:
+                        data_path = f"{base_folder}/model_outputs/model_{i}/{file_name}"
+                        if os.path.exists(data_path):
+                            aggregated_results[key][trial][data_name] = np.load(data_path)
+                
+                for data_name, file_name in [('min_distances', 'min_distances.npy'),
+                                             ('max_distances', 'max_distances.npy')]:
+                    data_path = f"{base_folder}/distances/{file_name}"
+                    if os.path.exists(data_path):
+                        aggregated_results[key][trial][data_name] = np.load(data_path)
+
+                # Clear memory for this iteration
+                del base_folder
+
+    if not os.path.exists("aggregated_results"):
+        os.makedirs("aggregated_results")
+    np.save(f"aggregated_results/data_input_dim_{input_dim}_partition_{partition}.npy", aggregated_results)
+    
+    # Clear memory
+    del aggregated_results
+    gc.collect()
+
+def process_combination(args):
+    input_dim, partition, trial_numbers, optimizers, pseudorehearsals = args
+    experiments = []
+    for trial in trial_numbers:
+        for optimizer in optimizers:
+            for use_pseudorehearsal in pseudorehearsals:
+                experiment_arguments = {
+                    'trial_number': trial,
+                    'input_dimension': input_dim,
+                    'partition_number': partition,
+                    'use_pseudorehearsal': use_pseudorehearsal,
+                    'loss_function': 'mse',
+                    'verbose': 0,
+                    'optimizer': optimizer
+                }
+                experiments.append(experiment_arguments)
+
+    with Pool() as pool:
+        pool.map(wrapper, experiments)
+
+    aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial_numbers)
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    trial_numbers = list(range(31))
+    optimizers = ['adam', 'sgd']
+    pseudorehearsals = [True, False]
+    combinations = [(input_dim, partition, trial_numbers, optimizers, pseudorehearsals)
+                    for input_dim in range(1, 7)
+                    for partition in range(1, 11)]
+
+    with Pool() as pool:
+        pool.map(process_combination, combinations)
+
+    print(f"Execution Time: {time.time() - start_time} seconds")
