13512e30f7f245cf5764b1f4cc092ad4b1121214
Heinrich van Deventer <51166531+hpdeventer@users.noreply.github.com>
2023-08-24 18:51:26 +0200

    Update perturb_experiment_main.py

--- perturb_experiment_main.py
+++ perturb_experiment_main.py
@@ -27,7 +27,81 @@ from concurrent.futures import ProcessPoolExecutor
 def wrapper(args):
     basic_perturbation_experiment(**args)
 
-def aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial_numbers):
+def load_and_save_data(input_dim=2, 
+                       partition=4, 
+                       use_pseudorehearsal=False, 
+                       optimizer='sgd', 
+                       trials=30, 
+                       num_models=6):
+    """
+    Load and save data from specified trials and models to a folder.
+    
+    Parameters:
+    - input_dim (int): Input dimension.
+    - partition (int): Partition number.
+    - use_pseudorehearsal (bool): Whether pseudo rehearsal is used.
+    - optimizer (str): Type of optimizer.
+    - trials (int): Number of trials.
+    - num_models (int): Number of models.
+    """
+
+    base_folder = f"results/input_dim_{input_dim}_partition_num_{partition}_\
+{use_pseudorehearsal}_{optimizer}"
+
+    # Initialize the main data storage with the desired structure
+    data = {
+        "min_distance": [],
+        "max_distance": []
+    }
+
+    for j in range(num_models):
+        data[f"model_{j}_perturbations"] = []
+
+    for i in range(trials):
+        trial_folder = f"{base_folder}/trial_{i}"
+
+        min_distance_path = f"{trial_folder}/distances/min_distances.npy"
+        max_distance_path = f"{trial_folder}/distances/max_distances.npy"
+
+        # Load the distances from the numpy files
+        if os.path.exists(min_distance_path):
+            min_distance = np.load(min_distance_path)
+            data["min_distance"].append(min_distance)
+        else:
+            print(f"Warning: {min_distance_path} not found.")
+
+        if os.path.exists(max_distance_path):
+            max_distance = np.load(max_distance_path)
+            data["max_distance"].append(max_distance)
+        else:
+            print(f"Warning: {max_distance_path} not found.")
+
+        for j in range(num_models):
+            perturbation_path = f"{trial_folder}/perturbations/model_{j}/absolute_perturbation.npy"
+
+            # Load the perturbation from the numpy file
+            if os.path.exists(perturbation_path):
+                perturbation_data = np.load(perturbation_path)
+                data[f"model_{j}_perturbations"].append(perturbation_data)
+            else:
+                print(f"Warning: {perturbation_path} not found.")
+
+    # Convert lists of numpy arrays to a single concatenated numpy array
+    for key in data:
+        if data[key]:  # Check if the list is not empty
+            data[key] = np.concatenate(data[key], axis=0).flatten()
+        else:
+            data[key] = None  # Set the value to None if the list is empty
+
+    # Save the data
+    save_folder = "aggregated_results"
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    save_path = f"{save_folder}/data_input_dim_{input_dim}_partition_{partition}.npy"
+    np.save(save_path, data)
+
+
+"""def aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial_numbers):
     aggregated_results = {}
 
     for trial in trial_numbers:
@@ -63,7 +137,7 @@ def aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial
     
     # Clear memory
     del aggregated_results
-    gc.collect()
+    gc.collect()"""
 
 
 def prepare_experiments(args):
@@ -106,8 +180,17 @@ if __name__ == "__main__":
         # Aggregate results after all experiments for the given partition have run
         for combo in input_dim_combinations:
             input_dim, _, _, _, _ = combo
-            aggregate_and_save(input_dim, partition, optimizers, pseudorehearsals, trial_numbers)
-
+            
+            # Loop through optimizers and pseudorehearsals to call the `load_and_save_data` for each combination
+            for optimizer in optimizers:
+                for use_pseudorehearsal in pseudorehearsals:
+                    load_and_save_data(input_dim=input_dim, 
+                                       partition=partition, 
+                                       optimizer=optimizer, 
+                                       use_pseudorehearsal=use_pseudorehearsal, 
+                                       trials=len(trial_numbers), 
+                                       num_models=6)
+                    
         # Cleanup to save memory
         del all_experiments
         gc.collect()
